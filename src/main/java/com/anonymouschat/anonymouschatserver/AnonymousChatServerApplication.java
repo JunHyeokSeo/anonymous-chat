724c991f96118fede1986d9dbc14fa23f2e62a51
JunHyeok Seo <sjhgd107@naver.com>
2025-07-18 14:36:16 +0900

    init

---
+++ src/main/java/com/anonymouschat/anonymouschatserver/AnonymousChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.anonymouschat.anonymouschatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymousChatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anonymouschat/anonymouschatserver/AnonymousChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymouschat.anonymouschatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
