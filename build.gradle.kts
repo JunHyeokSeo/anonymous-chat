25cd5442c72571cb0fc7213412f24e8ad252ab11
JunHyeok Seo <sjhgd107@naver.com>
2025-07-22 02:58:28 +0900

    chore : queryDsl 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,45 +18,58 @@ repositories {
 }
 
 dependencies {
-    // 웹 API 구성 (REST Controller 등)
+    // 웹 API 구성
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     // Spring Security + OAuth2 로그인
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-    // JWT 토큰 생성 및 검증용 라이브러리 (com.auth0)
+    // JWT
     implementation("com.auth0:java-jwt:4.4.0")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
-    // JPA + MySQL 연동
+    // JPA + MySQL
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
 
-    // 유효성 검증 (@Valid, @NotBlank 등)
+    // QueryDSL
+    implementation ("io.github.openfeign.querydsl:querydsl-jpa:7.0")
+    annotationProcessor ("io.github.openfeign.querydsl:querydsl-apt:7.0:jpa")
+    annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
+    annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
+
+    // 유효성 검증
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    // Swagger UI (API 문서 자동화)
+    // Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
     implementation("org.apache.commons:commons-lang3:3.18.0")
 
-    // 테스트 라이브러리
+    // Lombok
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // 테스트
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.mockito:mockito-core:5.10.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
+}
 
-    //lombok
-    implementation("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+val querydslDir = layout.buildDirectory.dir("generated/querydsl").get().asFile
 
-    // OAuth
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+sourceSets.main {
+    java.srcDir(querydslDir)
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.generatedSourceOutputDirectory.set(file("build/generated/querydsl"))
 }
 
 tasks.withType<Test> {
