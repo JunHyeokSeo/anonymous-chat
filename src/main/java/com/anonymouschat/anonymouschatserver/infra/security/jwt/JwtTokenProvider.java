0e35e9faf006a21fe9565778ec458c077617f9b5
JunHyeok Seo <sjhgd107@naver.com>
2025-08-15 17:40:50 +0900

    refactor: validate 책임 분리

--- src/main/java/com/anonymouschat/anonymouschatserver/infra/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/anonymouschat/anonymouschatserver/infra/security/jwt/JwtTokenProvider.java
@@ -2,12 +2,13 @@ package com.anonymouschat.anonymouschatserver.infra.security.jwt;
 
 import com.anonymouschat.anonymouschatserver.common.code.ErrorCode;
 import com.anonymouschat.anonymouschatserver.common.exception.auth.InvalidTokenException;
+import com.anonymouschat.anonymouschatserver.domain.type.OAuthProvider;
 import com.anonymouschat.anonymouschatserver.domain.type.UserRole;
 import com.anonymouschat.anonymouschatserver.infra.security.CustomPrincipal;
-import com.anonymouschat.anonymouschatserver.domain.type.OAuthProvider;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -27,6 +28,7 @@ public class JwtTokenProvider {
 	@Value("${jwt.refresh-token-expiration}")
 	private long refreshTokenValidityInSeconds;
 
+	@Getter
 	private Key secretKey;
 
 	@PostConstruct
@@ -34,7 +36,6 @@ public class JwtTokenProvider {
 		this.secretKey = Keys.hmacShaKeyFor(secretKeyPlain.getBytes());
 	}
 
-	//회원가입 전 토큰
 	public String createAccessTokenForOAuthLogin(OAuthProvider provider, String providerId) {
 		return createToken(builder -> builder
 				                              .claim("role", UserRole.ROLE_GUEST.getAuthority())
@@ -44,7 +45,6 @@ public class JwtTokenProvider {
 		);
 	}
 
-	//회원가입 완료 후 토큰 (userId 기반)
 	public String createAccessToken(Long userId, String role) {
 		return createToken(builder -> builder
 				                              .claim("role", role)
@@ -75,23 +75,13 @@ public class JwtTokenProvider {
 		return builder.signWith(secretKey, SignatureAlgorithm.HS256).compact();
 	}
 
-	public boolean validateToken(String token) {
-		try {
-			Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
-			return true;
-		} catch (SecurityException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException e) {
-			throw new InvalidTokenException(ErrorCode.INVALID_TOKEN, e);
-		} catch (ExpiredJwtException e) {
-			throw new InvalidTokenException(ErrorCode.EXPIRED_TOKEN, e);
-		}
-	}
-
 	public CustomPrincipal getPrincipalFromToken(String token) {
-		Claims c = parse(token);
-		Long userId = c.get("userId", Long.class);
-		String providerName = c.get("provider", String.class);
-		String providerId = c.get("providerId", String.class);
-		String role = c.get("role", String.class);
+		Claims claims = parse(token);
+
+		Long userId = claims.get("userId", Long.class);
+		String providerName = claims.get("provider", String.class);
+		String providerId = claims.get("providerId", String.class);
+		String role = claims.get("role", String.class);
 
 		if (userId != null) {
 			return CustomPrincipal.builder()
@@ -100,7 +90,6 @@ public class JwtTokenProvider {
 					       .build();
 		}
 
-		// OAuth 임시 사용자의 경우만 provider 정보 검사
 		if (providerName != null && providerId != null) {
 			return CustomPrincipal.builder()
 					       .provider(OAuthProvider.valueOf(providerName))
@@ -108,10 +97,17 @@ public class JwtTokenProvider {
 					       .role(role)
 					       .build();
 		}
+
 		throw new InvalidTokenException(ErrorCode.INVALID_TOKEN);
 	}
 
-	private Claims parse(String token) {
+	public long getExpirationMillis(String token) {
+		Claims claims = parse(token);
+		Date expiration = claims.getExpiration();
+		return expiration.getTime() - System.currentTimeMillis();
+	}
+
+	public Claims parse(String token) {
 		return Jwts.parserBuilder()
 				       .setSigningKey(secretKey)
 				       .build()
--- src/main/java/com/anonymouschat/anonymouschatserver/infra/security/jwt/JwtValidator.java
+++ src/main/java/com/anonymouschat/anonymouschatserver/infra/security/jwt/JwtValidator.java
@@ -2,6 +2,7 @@ package com.anonymouschat.anonymouschatserver.infra.security.jwt;
 
 import com.anonymouschat.anonymouschatserver.common.code.ErrorCode;
 import com.anonymouschat.anonymouschatserver.common.exception.auth.InvalidTokenException;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,15 @@ public class JwtValidator {
 	private final JwtTokenProvider jwtTokenProvider;
 
 	public void validate(String token) {
-		if (!jwtTokenProvider.validateToken(token)) {
-			throw new InvalidTokenException(ErrorCode.INVALID_TOKEN);
+		try {
+			Jwts.parserBuilder()
+					.setSigningKey(jwtTokenProvider.getSecretKey())
+					.build()
+					.parseClaimsJws(token);
+		} catch (SecurityException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+			throw new InvalidTokenException(ErrorCode.INVALID_TOKEN, e);
+		} catch (ExpiredJwtException e) {
+			throw new InvalidTokenException(ErrorCode.EXPIRED_TOKEN, e);
 		}
 	}
 }
