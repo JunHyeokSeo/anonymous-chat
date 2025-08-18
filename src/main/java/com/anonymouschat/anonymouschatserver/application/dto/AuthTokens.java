e463f912eb4a3344869a97552f59d04ac6c0bb20
JunHyeok Seo <sjhgd107@naver.com>
2025-08-19 02:59:36 +0900

    refactor : AuthUseCaseDto로 통합

--- src/main/java/com/anonymouschat/anonymouschatserver/application/dto/AuthTokens.java
+++
@@ -1,8 +0,0 @@
-package com.anonymouschat.anonymouschatserver.application.dto;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.annotation.Nullable;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public record AuthTokens(String accessToken, @Nullable String refreshToken) {
-}
